03cb87e94ee3aba09e05789b19cf4455a65eb4c9
pixelatedBrad <paras@sif.design>
2024-10-26 21:13:52 +0530

    error filled

---
+++ DTG/DTG.py
@@ -0,0 +1,45 @@
+import json
+from .classes.LLM import LLM
+from .prompts import prompt_funcs
+import time
+
+def get_dialogue_json(input_info):
+    t_0 = time.time()
+    starting_time = time.time()
+    performance_dict = {
+        'tags' : 0,
+        'abstract' : 0,
+        'endings' : 0,
+        'tree' : 0,
+        'formatted_json' : 0
+    }
+    llm = LLM()
+    print(f'Generating tags.')
+    tags_by_llm = llm.send_message_for_code(prompt_funcs.tag_prompt(input_info), model='gpt-4o-mini')
+    print(f'Tags generated in {time.time() - starting_time} seconds.')
+    performance_dict['tags'] = time.time() - starting_time
+    starting_time = time.time()
+    print(f'Generating abstract.')
+    abstract = llm.send_message_for_code(prompt_funcs.abstract_prompt(tags_by_llm, input_info))
+    print(f'Abstract generated in {time.time() - starting_time} seconds.')
+    performance_dict['abstract'] = time.time() - starting_time
+    starting_time = time.time()
+    print(f'Generating possible endings.')
+    endings_by_llm = llm.send_message_for_code(prompt_funcs.possible_ending_prompt(abstract), model='gpt-4o-mini')
+    print(f'Possible endings generated in {time.time() - starting_time} seconds.')
+    performance_dict['endings'] = time.time() - starting_time
+    starting_time = time.time()
+    print(f'Generating dialogue tree.')
+    tree_by_llm = llm.send_message_for_code(prompt_funcs.dialogue_prompt(abstract, endings_by_llm, tags_by_llm), model='o1-preview')
+    print(f'Dialogue tree generated in {time.time() - starting_time} seconds.')
+    performance_dict['tree'] = time.time() - starting_time
+    starting_time = time.time()
+    print(f'Formatting JSON.')
+    formatted_tree = llm.send_message(prompt_funcs.format_prompt(tree_by_llm), model='gpt-4o-mini')
+    print(f'JSON formatted in {time.time() - starting_time} seconds.')
+    performance_dict['formatted_json'] = time.time() - starting_time
+    starting_time = time.time()
+    print(f'Total time taken : {time.time() - t_0} seconds.')
+    print(f'Performance dict : {performance_dict}')
+    formatted_tree_json = json.loads(formatted_tree)
+    return formatted_tree_json
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,92 @@
 import streamlit as st
+from streamlit_flow import streamlit_flow
+from streamlit_flow.elements import StreamlitFlowNode, StreamlitFlowEdge
+from streamlit_flow.state import StreamlitFlowState
+from DTG import DTG
+from streamlit_flow.layouts import TreeLayout
+
+if 'npc_mood_text_input' not in st.session_state:
+    st.session_state['NPC Mood'] = 'happy'
+if 'npc_personality_text_input' not in st.session_state:
+    st.session_state['NPC Personality'] = 'friendly'
+if 'npc_convo_additional_flavour_text_input' not in st.session_state:
+    st.session_state['NPC Convo Additional Flavour'] = 'pickles, groves, and the like'
+
+npc_mood_text_input = st.text_input("NPC Mood", "happy", key='npc_mood_text_input')
+npc_personality_text_input = st.text_input("NPC Personality", "friendly", key='npc_personality_text_input')
+npc_convo_additional_flavour_text_input = st.text_input("NPC Convo Additional Flavour", "pickles, groves, and the like", key='npc_convo_additional_flavour_text_input')
+
+def process_json(json_data):
+    tree_nodes = []
+    tree_edges = []
+    dt_nodes_from_json = json_data['nodes']
+    for node in dt_nodes_from_json:
+        npc_node = StreamlitFlowNode(
+            id=node['id'],
+            pos=(0,0),
+            data={'content': node['npc_dialogue']},
+            draggable=True,
+            style={'backgroundColor': '#00c04b'}
+        )
+
+        tree_nodes.append(npc_node)
+        #check if there are no player choices
+        if node.get('player_choices') is None:
+            continue
+        if node['player_choices'] == []:
+            continue
+        for player_choice in node['player_choices']:
+            node = StreamlitFlowNode(
+                id=f"{node['id']}_|_{player_choice['next_node']}",
+                pos=(0, 0),
+                data={'content': player_choice['player_dialogue_choice']},
+                draggable=True
+            )
+            tree_nodes.append(node)
+
+            pe_id = f'{node["id"]}_e_{node['id']}_|_{player_choice['next_node']}'
+            pe_source = node['id']   
+            pe_target = f"{node['id']}_|_{player_choice['next_node']}"
+            p_edge = StreamlitFlowEdge(id=pe_id, source=pe_source, target=pe_target)
+
+            ne_id = player_choice['next_node']
+            ne_source = f"{node['id']}_|_{player_choice['next_node']}"
+            ne_target = player_choice['next_node']
+            n_edge = StreamlitFlowEdge(id=ne_id, source=ne_source, target=ne_target)
+
+            tree_edges.append(p_edge)
+            tree_edges.append(n_edge)
+    return tree_nodes, tree_edges
+
+flow_state = StreamlitFlowState([], [])
+new_state = streamlit_flow('fully_interactive_flow', 
+                flow_state, # Start with an empty state, or with some pre-initialized state
+                fit_view=True,
+                show_controls=True,
+                allow_new_edges=True,
+                animate_new_edges=True,
+                layout=TreeLayout("right"),
+                enable_pane_menu=True,
+                enable_edge_menu=True,
+                enable_node_menu=True,
+)
+
+def generate():
+    st.write(st.session_state)
+    input_info = {
+        'mood' : st.session_state.get('npc_mood_text_input'),
+        'personality' : st.session_state.get('npc_personality_text_input'),
+        'convo_additional_flavour' : st.session_state.get('npc_convo_additional_flavour_text_input')
+    }
+    dialogue_tree_json = DTG.get_dialogue_json(input_info)
+    dialogue_tree_nodes, dialogue_tree_edges = process_json(dialogue_tree_json)
+    if 'nodes' not in st.session_state.fully_interactive_flow:
+        st.session_state.fully_interactive_flow.nodes = []
+    if 'edges' not in st.session_state.fully_interactive_flow:
+        st.session_state.fully_interactive_flow.edges = []
+    st.session_state.fully_interactive_flow.nodes = dialogue_tree_nodes
+    st.session_state.fully_interactive_flow.edges = dialogue_tree_edges
+#add generate button and when every clicked run function
+st.write(st.session_state)
+generate_button = st.button("Generate", on_click=generate)
+
